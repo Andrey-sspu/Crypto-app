7a5bf588a5038dd7075dfab7cf6e753fb9ad4f09
Andrey <anticorruption.sspu@gmail.com>
2024-10-23 14:27:55 +0300

    second

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://andrey-sspu.github.io/Crypto-app/'
+  base: 'https://andrey-sspu.github.io/Crypto-app/',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 })
